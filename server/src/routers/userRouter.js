1571470e58acb3f8e57cc64d375515ebfcd1fcb8
Worty76 <lethanhdat762003@gmail.com>
2024-11-13 16:15:17 +0700

    feat: add routes

--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+router.get("/:userId", userController.getUserData);
+
 module.exports = router;
